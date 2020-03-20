68ce6e17a2c5f3f63507c4cc8331e0843ab02142
cricvk34 <48801532+cricvk34@users.noreply.github.com>
2020-03-20 10:49:59 +0530

    Update CHPINTU.cpp

--- CHPINTU.cpp
+++ CHPINTU.cpp
@@ -1,3 +1,28 @@
+/*Pintu sells M different types of fruits (numbered 1 through M). He sells them in N baskets (numbered 1 through N), 
+where for each valid i, the i-th basket costs Pi and it contains fruits of type Fi. Chef does not have too much money, 
+so he cannot afford to buy everything; instead, he wants to choose one of the M available types and purchase all the 
+baskets containing fruits of that type.
+Help Chef choose the type of fruit to buy such that he buys at least one basket and the total cost of the baskets he buys 
+is the smallest possible.*/
+Example Input
+1
+6 4
+1 2 3 3 2 2
+7 3 9 1 1 1
+Example Output
+5
+
+Example case 1:
+
+The sum of all baskets with fruits of type 1 is 7.
+The sum of all baskets with fruits of type 2 is 5.
+The sum of all baskets with fruits of type 3 is 10.
+The sum of all baskets with fruits of type 4 is 0 because there are no such baskets.
+ 
+	Hint: We can directly use Hash Map to solve the question
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
